672c69a41ab5050cff1a87e2e836641f994084a1
fuku-taro <fukumitsu.seko@gmail.com>
2024-12-15 15:58:26 +0900

    12/15

---
+++ resources/js/Pages/Inertia/Index.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+<!-- <h1>Inertiaテストです。</h1>
+<a href="/">aタグ経由です</a><br>
+<Link href="/">Link経由です</Link><br> -->
+aaaa
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+<h1>Inertiaテストです。</h1>
+<a href="/">aタグ経由です</a><br>
+<Link href="/">Link経由です</Link><br>
+<Link :href="route('inertia.index')">名前付きLink経由です</Link><br>
+</template>
