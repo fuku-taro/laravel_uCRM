f24a32730fc8d556132d69dfa7ad422264794fd5
fuku-taro <fukumitsu.seko@gmail.com>
2024-12-19 23:48:27 +0900

    郵便番号検索のライブラリの実装

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -4,6 +4,7 @@ import { Head } from '@inertiajs/inertia-vue3';
 import { reactive } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
+import { Core as YubinBangoCore } from "yubinbango-core2";
 
 
 defineProps({
@@ -14,6 +15,13 @@ const form = reactive({
   name: null, kana: null, tel: null, email: null, postcode: null,
   address: null, birthday: null, gender: null, memo: null
 })
+
+// 数字を文字に変換 第１引数が郵便番号、第２がコールバックで引数に住所
+const fetchAddress = () => {
+  new YubinBangoCore(String(form.postcode), (value) => {
+  form.address = value.region + value.locality + value.street
+})
+}
 
 const storeCustomer = () => {
   Inertia.post('/customers', form)
@@ -72,7 +80,7 @@ const storeCustomer = () => {
                           <div class="p-2 w-full">
                             <div class="relative">
                               <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label>
-                              <input type="number" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                              <input type="number" id="postcode" name="postcode" @change="fetchAddress" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
                           </div>
 
